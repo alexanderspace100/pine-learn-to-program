1b8a3beef31965884323a89a027149a489c83c2f
alexanderspace100 <alexanderspace100@gmail.com>
2016-08-19 20:43:08 +0300

    solved numbers

---
+++ numbers.rb
@@ -0,0 +1,14 @@
+#1. Hours in a year
+
+puts 365 * 24
+#=> 8760
+
+#2. Minutes in a decade
+
+puts 10 * 365 * 24 * 60
+#=> 5256000
+
+#3. The author's age with 1 025 million seconds old
+
+puts 1025000000/60/60/24/365
+#=> 32
